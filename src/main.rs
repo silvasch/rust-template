029179bbd93899924121ca5e041d3525ae6ccb82
mail@silvanschmidt.ch <mail@silvanschmidt.ch>
2025-03-21 12:04:08 +0100

    feat: add support for cargo-generate

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process::ExitCode;
 fn main() -> ExitCode {
     tracing_subscriber::fmt::init();
 
-    if let Err(e) = rust_template::run() {
+    if let Err(e) = {{ crate_name }}::run() {
         eprintln!("error: {}", e);
         return ExitCode::from(1);
     }
