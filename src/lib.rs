7ba599ca2280c4abc837c101583c433bbdc19e06
mail@silvanschmidt.ch <mail@silvanschmidt.ch>
2025-03-21 11:50:26 +0100

    feat: add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 mod error;
 pub(crate) use error::Error;
 
+pub(crate) mod prelude;
+use prelude::*;
+
 fn load_dotenv() -> Result<(), Error> {
     const TARGET: &str = "rust_bin_template::load_dotenv";
 
-    tracing::info!(target: TARGET, "loading a .env file");
+    info!(target: TARGET, "loading a .env file");
 
     match dotenvy::dotenv() {
         Ok(file_path) => {
-            tracing::info!(target: TARGET, "successfully loaded {}", file_path.display());
+            info!(target: TARGET, "successfully loaded {}", file_path.display());
         }
         Err(dotenvy::Error::Io(e)) if e.kind() == std::io::ErrorKind::NotFound => {
-            tracing::info!(target: TARGET, "could not found a .env file; skipping");
+            info!(target: TARGET, "could not found a .env file; skipping");
         }
         Err(dotenvy::Error::Io(e)) => return Err(Error::DotenvyIo(e)),
         Err(dotenvy::Error::LineParse(line_content, line_number)) => {
---
+++ src/prelude.rs
@@ -0,0 +1,5 @@
+#![allow(unused)]
+
+pub(crate) use tracing::{debug, error, info, warn};
+
+pub(crate) use crate::Error;
