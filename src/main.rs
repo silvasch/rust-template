cc73ac79b235dab1f6a452f6eb5772c4fc1701fb
mail@silvanschmidt.ch <mail@silvanschmidt.ch>
2025-03-21 11:52:01 +0100

    refactor: rename to rust-template

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process::ExitCode;
 fn main() -> ExitCode {
     tracing_subscriber::fmt::init();
 
-    if let Err(e) = rust_bin_template::run() {
+    if let Err(e) = rust_template::run() {
         eprintln!("error: {}", e);
         return ExitCode::from(1);
     }
